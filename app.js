d117e57a294e33808baf0927d881e2786f81dcb2
Kevin Navero <knavero@gmail.com>
2018-07-18 16:51:58 -0700

    express local library tutorial init

---
+++ app.js
@@ -0,0 +1,52 @@
+let createError = require('http-errors');
+let express = require('express');
+let mongoose = require('mongoose');
+let local_library_db = 'mongodb://127.0.0.1:27017/local_library';
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let process = require('process');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
+mongoose.connect(local_library_db, { useNewUrlParser: true })
+   .catch(err => {
+      console.error(`MongoDB connection error: ${err}`);
+      process.exit(1);
+   });
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/cool', function(req, res, next) {
+   res.send("You're so cool");
+});
+
+module.exports = router;
