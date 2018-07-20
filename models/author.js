fc4861124271adfc75d5da9cba4c906c37fa0464
Kevin Navero <knavero@gmail.com>
2018-07-20 14:50:44 -0700

    adding models

---
+++ models/author.js
@@ -0,0 +1,19 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let AuthorSchema = new Schema({
+   first_name: { type: String, require: true, max: 100 },
+   family_name: { type: String, require: true, max: 100 },
+   date_of_birth: { type: Date },
+   date_of_death: { type: Date }
+})
+
+AuthorSchema
+   .virtual('name')
+   .get(function() { return this.family_name + ', ' + this.first_name });
+
+AuthorSchema
+   .virtual('url')
+   .get(function() { return '/catalog/author/' + this._id });
+
+module.exports = mongoose.model('Author', AuthorSchema);
