e1ac777d7285ca0a32a9823bf5c3a0f1c1c7ad72
Kevin Navero <knavero@gmail.com>
2018-07-25 19:01:00 -0700

    home page added

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,23 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+const async = require('async');
 
 exports.index = function(req, res) {
-   res.render('index', { title: "Site Home Page" });
+   async.parallel({
+      bookCount: cb => Book.countDocuments({}, cb),
+      bookInstanceCount: cb => BookInstance.countDocuments({}, cb),
+      bookInstanceAvailableCount: 
+         cb => BookInstance.countDocuments({ status: 'Available' }, cb),
+      authorCount: cb => Author.countDocuments({}, cb),
+      genreCount: cb => Genre.countDocuments({}, cb) 
+   },
+   (err, results) => res.render('index', {
+      title: 'Local Library Home',
+      error: err,
+      data: results
+   }));
 };
 
 // display list of all books
