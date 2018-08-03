bf16fe63311cbc10f2a93de2510d65bb1f3089ad
Kevin Navero <knavero@gmail.com>
2018-08-03 12:08:35 -0700

    author detail page added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
 const Author = require('../models/author');
+const Book = require('../models/book');
+const async = require('async');
+const createError = require('http-errors');
 
 // display list of all Authors
 exports.authorList = function(req, res, next) {
@@ -17,8 +20,28 @@ exports.authorList = function(req, res, next) {
 };
 
 // display detail page for a specific Author
-exports.authorDetail = function(req, res) {
-   res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.authorDetail = function(req, res, next) {
+   async.parallel(
+      {
+         author: cb => Author.findById(req.params.id, cb),
+         books: cb => 
+            Book
+               .find({ author: req.params.id }, 'title summary')
+               .sort('title').exec(cb)
+      },
+      (err, results) => { 
+         if (err) next(err);
+         if (!results.author) next(createError(404, 'Author not found'));
+         res.render(
+            'authorDetail', 
+            { 
+               title: 'Author Detail',
+               author: results.author,
+               books: results.books
+            }
+         );
+      }
+   );
 };
 
 // display Author create form on GET
