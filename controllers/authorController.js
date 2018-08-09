88dfc08bd5c97161fdd9a39276d51f32ca13a21a
Kevin Navero <knavero@gmail.com>
2018-08-09 03:50:11 -0700

    create author get and post added

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,6 +2,8 @@ const Author = require('../models/author');
 const Book = require('../models/book');
 const async = require('async');
 const createError = require('http-errors');
+const { body, validationResult} = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // display list of all Authors
 exports.authorList = function(req, res, next) {
@@ -46,13 +48,58 @@ exports.authorDetail = function(req, res, next) {
 
 // display Author create form on GET
 exports.authorCreateGet = function(req, res) {
-   res.send('NOT IMPLEMENTED: Author create GET');
+   res.render('authorCreate', { title: 'Create Author'});
 };
 
 // handle Author create on POST
-exports.authorCreatePost = function(req, res) {
-   res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.authorCreatePost = [
+   body('first_name', "first name required")
+      .trim().isLength({ min: 1 }).escape(),
+   body('last_name', "last name required")
+      .trim().isLength({ min: 1 }).escape(),
+   body('date_of_birth')
+      .optional({ checkFalsy: true }).isBefore().withMessage(
+         "date of birth must be equal to or before current date"),
+   body('date_of_death')
+      .optional({ checkFalsy: true }).isBefore().withMessage(
+         "date of death must be equal to or before current date")
+      .custom((date_of_death, { req }) => 
+         new Date(date_of_death) >= new Date(req.body.date_of_birth)
+      )
+      .withMessage("date of death must be equal to or after date of birth"),
+   function(req, res, next) {
+      let errors = validationResult(req);
+
+      let newAuthor = new Author({
+         first_name: req.body.first_name,
+         family_name: req.body.last_name,
+         date_of_birth: req.body.date_of_birth,
+         date_of_death: req.body.date_of_death
+      });
+
+      if (!errors.isEmpty()) {
+         res.render('authorCreate', {
+            title: 'Create Author',
+            author: newAuthor,
+            errors: errors.array()
+         });
+      }
+      else {
+         Author.findOne({ 
+            first_name: newAuthor.first_name, 
+            family_name: newAuthor.family_name })
+            .then(author => {
+               if (author) return author
+               else {
+                  newAuthor.save();
+                  return newAuthor;
+               }
+            })
+            .then(author => res.redirect(author.url))
+            .catch(err => next(err));
+      }
+   }
+]
 
 // display Author delete form on GET
 exports.authorDeleteGet = function(req, res) {
