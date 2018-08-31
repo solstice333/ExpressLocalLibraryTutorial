aeffdeeec0e8d5753f3f3099006e01e5e4989dab
Kevin Navero <knavero@gmail.com>
2018-08-31 16:18:33 -0700

    author update post

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -185,6 +185,56 @@ exports.authorUpdateGet = function(req, res, next) {
 };
 
 // handle Author update on POST
-exports.authorUpdatePost = function(req, res) {
-   res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.authorUpdatePost = [
+   body('first_name')
+      .trim().isLength({ min: 1 }).withMessage("first name required")
+      .isAlphanumeric().withMessage(
+         "first name must only have alphanumeric characters")
+      .escape(),
+   body('last_name')
+      .trim().isLength({ min: 1 }).withMessage("last name required")
+      .isAlphanumeric().withMessage(
+         "last name must only have alphanumeric characters")
+      .escape(),
+   body('date_of_birth')
+      .optional({ checkFalsy: true }).isISO8601()
+      .customSanitizer(toTZOffsettedDate)
+      .isBefore().withMessage(
+         "date of birth must be equal to or before current date")
+      .toDate(),
+   body('date_of_death')
+      .optional({ checkFalsy: true }).isISO8601()
+      .customSanitizer(toTZOffsettedDate)
+      .isBefore().withMessage(
+         "date of death must be equal to or before current date")
+      .custom((dod, { req }) => {
+         let dob = req.body.date_of_birth || null; 
+         return dob ? dod >= dob : true;
+      })
+      .withMessage("date of death must be equal to or after date of birth")
+      .toDate(),
+   (req, res, next) => {
+      let errors = validationResult(req);
+
+      let updatedAuthor = new Author({
+         _id: req.params.id,
+         first_name: req.body.first_name,
+         family_name: req.body.last_name,
+         date_of_birth: req.body.date_of_birth,
+         date_of_death: req.body.date_of_death
+      });
+
+      if (!errors.isEmpty()) {
+         res.render('authorForm', {
+            title: 'Update Author',
+            author: updatedAuthor,
+            errors: errors.array()
+         });
+      }
+      else {
+         Author.findByIdAndUpdate(req.params.id, updatedAuthor)
+            .then(() => res.redirect(updatedAuthor.url))
+            .catch(next);
+      }
+   }
+];
